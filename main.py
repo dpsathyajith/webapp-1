63097726395580040839f283a787fdd22c5fd4a7
satyajitdp <satyajitdp@gmail.com>
2026-04-04 10:07:57 +0530

    a3

--- main.py
+++ main.py
@@ -5,30 +5,24 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 
 # Templates folder
-
 templates = Jinja2Templates(directory="templates")
 
 # Home page (UI)
 @app.get("/")
 @app.get("/home", response_class=HTMLResponse)
 def home(request: Request):
-return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 # Optional API (future use)
-
 @app.get("/cakes")
 def get_cakes():
-return [
-{"name": "Chocolate Cake", "price": 500},
-{"name": "Red Velvet", "price": 600}
-]
+    return [
+        {"name": "Chocolate Cake", "price": 500},
+        {"name": "Red Velvet", "price": 600}
+    ]
 
 # Optional: Order endpoint (future)
-
 @app.post("/order")
 def create_order(order: dict):
-print("Order received:", order)
-return {"message": "Order placed successfully 🎉"}
-
-
-
+    print("Order received:", order)
+    return {"message": "Order placed successfully 🎉"}
