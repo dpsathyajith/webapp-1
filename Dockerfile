2c5ac5af7bb4887fca0e3cba655d78cc6474d723
satyajitdp <satyajitdp@gmail.com>
2026-04-04 09:37:28 +0530

    a2

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,18 @@ FROM python:3.11-slim
 
 WORKDIR /app
 
-COPY requirements.txt .
+# Copy files
+
+COPY . /app
+
+# Install dependencies
+
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY . .
+# Expose port
+
+ENV PORT=8080
+
+# Start app
 
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/home")
-def home():
-    return {"message": "Hello .... from Cloud Run 🚀"}
+# Templates folder
+
+templates = Jinja2Templates(directory="templates")
+
+# Home page (UI)
+
+@app.get("/home", response_class=HTMLResponse)
+def home(request: Request):
+return templates.TemplateResponse("index.html", {"request": request})
+
+# Optional API (future use)
+
+@app.get("/cakes")
+def get_cakes():
+return [
+{"name": "Chocolate Cake", "price": 500},
+{"name": "Red Velvet", "price": 600}
+]
+
+# Optional: Order endpoint (future)
+
+@app.post("/order")
+def create_order(order: dict):
+print("Order received:", order)
+return {"message": "Order placed successfully 🎉"}
+
+
+
